3ff5416f76a3a063d5d97e0cb64addbb09396eac
Dennis Zimmer <dennis.zimmer@mnd.thm.de>
2020-08-05 15:15:49 +0200

    TestMain added Comments and Example Files to download

--- src/main/java/PublicTransportRouting/TestMain.java
+++ src/main/java/PublicTransportRouting/TestMain.java
@@ -1,40 +1,63 @@
 package PublicTransportRouting;
 
 import PublicTransportRouting.Test.Route_Loader;
-import PublicTransportRouting.supClass.FileDownloader;
 import PublicTransportRouting.supClass.Location;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.time.LocalDateTime;
 
+/**
+ * Example Class to how to handle the module
+ * - has examples for ever funktion of the Facade Class
+ * - creating Graph
+ * - loading Graph
+ * - creating an Query
+ * - loading a Route from a file
+ * <p>
+ * below are some URL for testing (at end of the class)
+ */
 public class TestMain {
+    //---------------------------------------- Main Methode -----------------------------------------//
     public static void main(String[] args) {
-        TestMain lul = new TestMain();
-        lul.machwas();
-//        lul.loadTest();
+        TestMain testMain = new TestMain();
+        testMain.doAll();
+//        testMain.loadTest();
+    }
+    //----------------------------------------- Constructor -----------------------------------------//
+
+    public void doAll() {
+        //Download Files from an URL link (OSM and GTFS Files) if non is available locally
+        //(Folder to save: resources/OSM_Files and resources/GTFS_Files)
+        //Example given is for Berlin !!!
+//        PT_Facade_Class.downloadFiles("https://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf",
+//                                       "http://download.vrsinfo.de/gtfs/GTFS_VRS_mit_SPNV.zip");
+
+        //Creating a Graph if this exact Graph isn´t already build
+        //(Folder to save: resources/graph/ Name of the Osm File + _with_Transit)
+        //Example given is for Berlin with the associated Gtfs File
+//        PT_Facade_Class.createGraph("nordrhein-westfalen-latest.osm.pbf","GTFS_VRS_mit_SPNV.zip");
+
+        //loading an existing Graph from an folder
+        //(Folder where the graphs are saved: resources/graph)
+        PT_Facade_Class pt = new PT_Facade_Class("Europe/Paris", 2020, 7, 31, 13, 0, "JSON");
+        pt.loadGraph("nordrhein-westfalen-latest.osm.pbf_with_Transit");
+
+        //Creating a Query on a loaded Graph
+        //Saves returning Routes in resources/Routes
+        pt.ptRouteQuery(new Location(50.963189, 6.985617), new Location(50.943347, 6.982131),
+                LocalDateTime.of(2020, 7, 31, 13, 0), "all");
+
+        //Optional second or Xth Query
+        pt.ptRouteQuery(new Location(50.962427, 6.980996), new Location(50.941313, 6.958251),
+                LocalDateTime.of(2020, 7, 31, 14, 0), "all");
+
     }
 
-    public void machwas(){
-
-        //Dateien herunterladen, welche zum erstellen vom Graph gebruacht wird (Beispiel ist hier Berlin)
-        PT_Facade_Class.downloadFiles("https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf",
-                                       "https://www.vbb.de/media/download/2029/gtfs-vbb.zip");
-
-        //Einen nicht vorhandenen Graph erstellen
-        PT_Facade_Class.createGraph("berlin-latest.osm.pbf","gtfs-vbb.zip");
-
-        //Einen bereits vorhandenen Graph laden
-        PT_Facade_Class pt = new PT_Facade_Class("Europe/Paris",2020,7,31,13,00,"JSON");
-        pt.loadGraph("berlin-latest.osm.pbf_with_Transit");
-
-        //Stelle eine Abfrage um eine Route zu erhalten
-        pt.ptRouteQuery(new Location(52.640251,13.207669),new Location(52.518036,13.431559),
-                                         LocalDateTime.of(2020,7,31,13,00),"all");
-
-    }
-
-    public void loadTest(){
+    /*
+    Loading an existing Route
+    On the loaded Route is it possible to make some test or queries
+     */
+    public void loadTest() {
         String FilePath;
         FilePath = "src\\main\\resources\\Routes\\RoutenListe.json";
         Route_Loader loader = new Route_Loader();
@@ -46,11 +69,18 @@ public class TestMain {
         }
         loader.test();
     }
+    //--------------------------------------- Getter & Setter ---------------------------------------//
+    //----------------------------------------- Additional ------------------------------------------//
 }
+    /*
+    -----------------------------------------------------------------------------------------------------------
+    TEST FILES:
+    OSM download from --> https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf                            --> Berlin
+                          https://download.geofabrik.de/europe/germany/nordrhein-westfalen-latest.osm.pbf               --> NRW
+
+    GTFS download from -->https://www.vbb.de/media/download/2029/GTFS.zip                                               --> Berlin
+                          http://download.vrsinfo.de/gtfs/GTFS_VRS_mit_SPNV.zip                                         --> NRW
+     -----------------------------------------------------------------------------------------------------------
+     */
 
 
-//------------------------------------------ Variable -------------------------------------------//
-//----------------------------------------- Constructor -----------------------------------------//
-//------------------------------------------- Methods -------------------------------------------//
-//--------------------------------------- Getter & Setter ---------------------------------------//
-//----------------------------------------- Additional ------------------------------------------//
