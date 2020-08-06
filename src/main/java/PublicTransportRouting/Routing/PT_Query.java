5240d8ba81c2a6a62b3db698c3dfdea8cd470aa3
Dennis Zimmer <dennis.zimmer@mnd.thm.de>
2020-08-06 13:15:52 +0200

    now calculate Routes considering the Transfers too

--- src/main/java/PublicTransportRouting/Routing/PT_Query.java
+++ src/main/java/PublicTransportRouting/Routing/PT_Query.java
@@ -1,11 +1,8 @@
 package PublicTransportRouting.Routing;
 
 import PublicTransportRouting.supClass.Location;
-import com.graphhopper.config.Profile;
 import com.graphhopper.reader.gtfs.Request;
-import com.graphhopper.routing.weighting.QueryGraphWeighting;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -26,13 +23,13 @@ public class PT_Query {
     /*
     Method to create the request with given parameters and then returns the request
      */
-    // TODO HIER MAL SCHAUEN OB ICH HIER NOCH WEITERE CONTRAINS BZW. EINSTELLUNGEN SETZEN KANN, WELCHE DIE SUCHE WEITER EINSCHRÄNKEN ODER BESSERE MULTICRITERIA ROUTING ERMÖGLICHEN
     private Request createRequest(Location from, Location to, LocalDateTime dateTime, ZoneId zoneId){
         Request request = new Request(from.getLat(), from.getLon(), to.getLat(), to.getLon());  //creating the graphhopper request
-        request.setEarliestDepartureTime(dateTime.atZone(zoneId).toInstant());               //setting the start time for the earliest departure time within a time zone
-        request.setLimitSolutions(5);              //setting a maximum of resulting Routes
+        request.setEarliestDepartureTime(dateTime.atZone(zoneId).toInstant());                  //setting the start time for the earliest departure time within a time zone
         request.setProfileQuery(true);
-        request.setIgnoreTransfers(true);
+        request.setIgnoreTransfers(false);         //Transfers are taken into consideration
+        request.setLimitSolutions(5);              //setting a maximum of resulting Routes
+
 
         return request;
     }
