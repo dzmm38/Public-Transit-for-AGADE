761f617633e9744b86c2155fa5fb8ecb3516104b
Dennis Zimmer <dennis.zimmer@mnd.thm.de>
2020-08-06 13:29:19 +0200

    the loader for YAML an JSON Routes should now work correctly changed the console output to print the route criteria added getter & setter for the route Array

--- src/main/java/PublicTransportRouting/Test/Route_Loader.java
+++ src/main/java/PublicTransportRouting/Test/Route_Loader.java
@@ -1,24 +1,22 @@
 package PublicTransportRouting.Test;
 
-import PublicTransportRouting.Routemodel.PT_Leg;
 import PublicTransportRouting.Routemodel.PT_Route;
-import PublicTransportRouting.Routemodel.PT_Stop;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Class to Load a saved Rout in either JSON or YAML
  */
 public class Route_Loader {
     //------------------------------------------ Variable -------------------------------------------//
-    PT_Route route = null;
+    PT_Route[] routeArray;
 
     //----------------------------------------- Constructor -----------------------------------------//
     //------------------------------------------- Methods -------------------------------------------//
-
     /**
      * Methode to load an existing Route from a file
      *
@@ -30,12 +28,13 @@ public class Route_Loader {
         System.out.println("Reading Route from file ..........");
         ObjectMapper om;
 
+        //Checks if the File is an YAML or JSON File
         if (FilePath.contains(".yml")) {
             om = new ObjectMapper(new YAMLFactory());
-            route = om.readValue(new File(FilePath), PT_Route.class);
+            routeArray = om.readValue(new File(FilePath), PT_Route[].class);    //reading the YAML file in an array, every entry represents a Route
         } else if (FilePath.contains(".json")) {
             om = new ObjectMapper(new JsonFactory());
-            route = om.readValue(new File(FilePath), PT_Route.class);
+            routeArray = om.readValue(new File(FilePath), PT_Route[].class);    //reading the JSON file in an array, every entry represents a Route
         } else {
             System.out.println("File type could not be found please use .yml an .json only");
         }
@@ -45,46 +44,26 @@ public class Route_Loader {
      * Method to test if the loading was successful by printing out text to the console
      */
     public void test() {
-        for (PT_Leg leg : route.getLegs()) {
-            if (leg != null) {
-                System.out.println("-------------------------------------------------------------");
-                System.out.println(leg.getLegId());
-                System.out.println(leg.getStartLocation().getLat() + "," + leg.getStartLocation().getLon());
-                System.out.println(leg.getEndLocation().getLat() + " " + leg.getEndLocation().getLon());
-                System.out.println(leg.getLegStartTime());
-                System.out.println(leg.getLegEndTime());
-                System.out.println(leg.getLegType());
-                System.out.println(leg.getStopCounter());
-                System.out.println(leg.getStops());
-                System.out.println("-------------------------------------------------------------");
-            } else {
-                System.out.println("-------------------------------------------------------------");
-                System.out.println("Das hier ist ein walk Leg");
-                System.out.println("-------------------------------------------------------------");
-            }
-        }
-        for (PT_Stop stop : route.getStops()) {
-            System.out.println("-------------------------------------------------------------");
-            System.out.println(stop.getStopId());
-            System.out.println(stop.getLocation().getLocationName());
-            System.out.println(stop.getLocation().getLat() + "," + stop.getLocation().getLon());
-            System.out.println("Zeiten: " + stop.getArrivalTime() + "  " + stop.getDepartureTime());
-            System.out.println("Tick´s: " + stop.getArrivalTick() + "  " + stop.getDepartureTick());
-            System.out.println("-------------------------------------------------------------");
-        }
-        System.out.println(route.getStops().size());
+        Arrays.stream(routeArray).forEach(testroute -> {
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.println("ArrivalTime:     " + testroute.getArrivalTime());
+            System.out.println("Route Duration:  " + testroute.getDurationInMin());
+            System.out.println("Transfers:       " + testroute.getTransfers());
+            System.out.println("Walk Distance:   " + testroute.getWalkDistanceInMeters());
+            System.out.println("Kosten der Route:" + testroute.getCost());
+            System.out.println("Anzahl Stops:    " + testroute.getStopCounter());
+            System.out.println("-------------------------------------------------------------------------------------");
+        });
     }
 
     //--------------------------------------- Getter & Setter ---------------------------------------//
-    public PT_Route getRoute() {
-        return route;
+    public PT_Route[] getRouteArray() {
+        return routeArray;
     }
 
-    public void setRoute(PT_Route route) {
-        this.route = route;
+    public void setRouteArray(PT_Route[] routeArray) {
+        this.routeArray = routeArray;
     }
 
     //----------------------------------------- Additional ------------------------------------------//
-
-    //TODO Things to to with the loaded Route can be written here
 }
